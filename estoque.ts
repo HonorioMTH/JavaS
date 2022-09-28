5d70eab5ffbadf97944c5f81fc005bc03f287b08
HonorioMTH <matheus.honorio88@gmail.com>
2022-09-28 14:06:33 -0300

    22_09

---
+++ estoque.ts
@@ -0,0 +1,8 @@
+let estoque: string[] = []
+
+estoque.push('tênis', 'play5', 'controle', 'playmovie');
+
+console.log(estoque[2]);
+
+estoque.splice(1, 1);
+console.log(estoque)
---
+++ programsaude.ts
@@ -0,0 +1,3 @@
+let imc = (alt: number, peso: number) => peso / (alt*alt)
+
+console.log(imc(1.67, 75).toFixed(1));
