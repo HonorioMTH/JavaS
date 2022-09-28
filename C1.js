aa49e67f900382e0de214c00f9c29e56d2e4891d
HonorioMTH <matheus.honorio88@gmail.com>
2022-09-28 14:40:42 -0300

    Exercicios do dia 13_09

---
+++ C1.js
@@ -0,0 +1,11 @@
+let n = 233
+
+do {
+    alert (n)
+
+if (n < 300 || n > 400) {
+    n = parseInt(n) + parseInt(3)
+} else {
+    n = n + 5
+}
+} while (n != 456)
---
+++ C2.js
@@ -0,0 +1,15 @@
+let n, n2 = 1, n3 = 0, s
+
+n = prompt("Digite um número: ")
+
+do {
+
+alert (n2)
+n3 = parseInt(n3) + parseInt(n2)
+n2 = n2 + 1
+
+if (n == n2){
+s = parseInt(n3) + parseInt(n)
+alert (s)
+}
+}while (n2 < n)
